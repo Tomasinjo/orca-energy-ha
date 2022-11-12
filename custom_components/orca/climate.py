7b43623ed1edd9d15148d7107e9fff56d3354783
Robert <robert.slak@gmail.com>
2022-11-12 22:47:00 +0100

    Support for turning heating off and HVACAction - current state. In case 24h schedule is selected the target temp is used, otherwise high/low

--- custom_components/orca/climate.py
+++ custom_components/orca/climate.py
@@ -3,7 +3,7 @@ from logging import getLogger
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.core import callback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.components.climate.const import HVACMode
+from homeassistant.components.climate.const import HVACMode, HVACAction, ClimateEntityFeature
 
 _LOGGER = getLogger(__name__)
 DOMAIN = 'orca'
@@ -17,7 +17,14 @@ MODE_MAPPING = {
 REVERSE_MODE_MAPPING = {
     'heat_cool': 3,
     'cool': 2,
-    'heat': 1
+    'heat': 1,
+    'off': 0
+}
+
+ACTION_MAPPING = {
+    'idle': HVACAction.IDLE,
+    'heating': HVACAction.HEATING,
+    'cooling': HVACAction.COOLING
 }
 
 
@@ -40,29 +47,44 @@ class OrcaClimate(CoordinatorEntity, ClimateEntity):
         self._attr_unique_id = 'b53d1ef8-17b9-49ca-8c82-73f16a4b9b5a'
         self._attr_temperature_unit = TEMP_CELSIUS
         self._attr_current_temperature = float()
-        #self._attr_target_temperature = float()
+        self._attr_target_temperature = float()
         self._attr_target_temperature_high = float()
         self._attr_target_temperature_low = float()
         self._attr_target_temperature_step = 0.1
         self._attr_hvac_mode = HVACMode.HEAT_COOL
         self._attr_hvac_modes = [HVACMode.OFF, HVACMode.HEAT, HVACMode.COOL, HVACMode.HEAT_COOL]
-        #self._attr_hvac_action = ''  # todo current hvac state  
-        self._attr_supported_features = 3
+        self._attr_hvac_action = HVACAction.IDLE 
+        self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.TARGET_TEMPERATURE_RANGE
 
     @callback
     def _handle_coordinator_update(self) -> None:
         _LOGGER.debug(f"climate callback update")
         """Handle updated data from the coordinator."""
         self._attr_current_temperature = self.coordinator.data['2_Temp_Prostora'].value
-        #self._attr_target_temperature = self.coordinator.data['2_Temp_prostor_dnevna'].value
-        self._attr_target_temperature_high = self.coordinator.data['2_Temp_prostor_dnevna'].value
-        self._attr_target_temperature_low  = self.coordinator.data['2_Temp_prostor_nocna'].value
-        self._attr_hvac_mode = MODE_MAPPING.get(self.coordinator.data['2_Rezim_MK1'].value)
+
+        floor_heating_24h_schedule = self.coordinator.data['2_Timer_MK1_24_ur'].value
+        if floor_heating_24h_schedule:
+            self._attr_target_temperature = self.coordinator.data['2_Temp_prostor_dnevna'].value
+            self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
+        else:
+            self._attr_target_temperature_high = self.coordinator.data['2_Temp_prostor_dnevna'].value
+            self._attr_target_temperature_low  = self.coordinator.data['2_Temp_prostor_nocna'].value
+            self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE_RANGE
+
+        self._attr_hvac_action = ACTION_MAPPING.get(self.coordinator.data['2_Rezim_delov_TC'].value)
+        
+        # set based on mode only if HP is turned on
+        if self.coordinator.data['2_MK1_vklop'].value == True:
+            self._attr_hvac_mode = MODE_MAPPING.get(self.coordinator.data['2_Rezim_MK1'].value)
+        else:
+            self._attr_hvac_mode = HVACMode.OFF
         _LOGGER.debug(f"""Climate update: 
         _attr_current_temperature: {self._attr_current_temperature}
         self._attr_target_temperature_high: {self._attr_target_temperature_high}
         self._attr_target_temperature_low: {self._attr_target_temperature_low}
-        self._attr_hvac_mode: {self._attr_hvac_mode}""")
+        self._attr_target_temperature: {self._attr_target_temperature}
+        self._attr_hvac_mode: {self._attr_hvac_mode}
+        self._attr_hvac_action: {self._attr_hvac_action}""")
         self.async_write_ha_state()
 
 
@@ -75,6 +97,8 @@ class OrcaClimate(CoordinatorEntity, ClimateEntity):
             resp = self.orca.set_value('2_Temp_prostor_nocna', temperature)
         if temperature := kwargs.get('target_temp_high', 0) * 10:
             resp = self.orca.set_value('2_Temp_prostor_dnevna', temperature)
+        if temperature := kwargs.get('temperature', 0) * 10:
+            resp = self.orca.set_value('2_Temp_prostor_dnevna', temperature)
         _LOGGER.debug(f'Set temperature response: {resp}')
         self.coordinator.async_refresh()
 
@@ -82,19 +106,16 @@ class OrcaClimate(CoordinatorEntity, ClimateEntity):
     def set_hvac_mode(self, hvac_mode) -> None:
         _LOGGER.debug(f"climate setting mode: mode={hvac_mode}")
         mode = REVERSE_MODE_MAPPING.get(hvac_mode)
+
         if mode is None:
             _LOGGER.error(f'Unknown mode: {hvac_mode}')
             return
-        resp = self.orca.set_value('2_Rezim_MK1', mode)
+
+        # in case hp is turned off this is set with different value
+        if mode == 0:
+            resp = self.orca.set_value('2_MK1_vklop', 0)
+        else:
+            self.orca.set_value('2_MK1_vklop', 1)
+            resp = self.orca.set_value('2_Rezim_MK1', mode)
         _LOGGER.debug(f'Set mode response: {resp}')
         self.coordinator.async_refresh()
-
-
-
-
-
-
-
-
-
-
