18255a8051a729b191a260d2b16f91cfde3cbfa7
Tomasinjo <43334457+Tomasinjo@users.noreply.github.com>
2023-05-08 17:19:31 +0200

    changed to async_forward_entry_setups

--- custom_components/orca-ha/__init__.py
+++ custom_components/orca-ha/__init__.py
@@ -9,7 +9,10 @@ _LOGGER = getLogger(__name__)
 PLATFORMS = ["sensor", "binary_sensor", "climate"]
 DOMAIN = 'orca'
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(
+    hass: HomeAssistant, 
+    entry: ConfigEntry,
+    ):
     _LOGGER.info("Creating setup entry")
     user = entry.data["username"]
     passwd = entry.data["password"]
@@ -20,8 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     hass.data[DOMAIN]['orca_obj'] = orca
-
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
